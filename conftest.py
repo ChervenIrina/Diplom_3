27f8613190aaefafac62352cae5464bbffe862c6
gorbunova_is <gorbunova_is@magnit.ru>
2025-02-11 22:20:58 +0300

    Diplom3 commit

--- conftest.py
+++ conftest.py
@@ -1,16 +1,26 @@
 import allure
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.service import Service
 
 from helpers_API import create_user, generate_new_user, delete_user
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(params=["chrome", "firefox"])
 @allure.title('Фикстура создает драйвер')
-def driver():
-    browser = webdriver.Chrome()
-    yield browser
-    browser.quit()
+def driver(request):
+    if request.param == "chrome":
+        driver = webdriver.Chrome()
+    elif request.param == "firefox":
+        options = Options()
+        options.binary = FirefoxBinary(r'E:\users\gorbunova_is\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\firefox.exe')
+        driver = webdriver.Firefox(
+            service=Service(executable_path='E:/firefox/geckodriver.exe'),
+            options=options)
+    yield driver
+    driver.quit()
 
 @pytest.fixture
 @allure.title('Фикстура создает/удаляет нового пользователя через API')
